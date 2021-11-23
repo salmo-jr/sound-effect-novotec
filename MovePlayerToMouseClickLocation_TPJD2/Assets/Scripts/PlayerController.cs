f44f58685ad82a5790bfd649b6c62dfdc541ffce
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2021-11-23 20:53:23 -0200

    adds sound effect on player

--- MovePlayerToMouseClickLocation_TPJD2/Assets/Scripts/PlayerController.cs
+++ MovePlayerToMouseClickLocation_TPJD2/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 4f;
+    public float initialVolume = 0.4f;
+    public Boundary boundary;
 
     private Animator anim;
+    private AudioSource audioSource;
+    private Vector3 target;
     private float lastX, lastY;
     private float inputH, inputV;
     private float dt;
-    private Vector3 target;
-    
+    private float maiorVolume;
+    private float menorVolume = 0;
+    private float menorPan = -0.8f;
+    private float maiorPan = 0.8f;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        maiorVolume = 1.0f - initialVolume;
     }
 
     // Update is called once per frame
@@ -45,15 +60,24 @@ public class PlayerController : MonoBehaviour
             anim.SetFloat("LastX", lastX);
             anim.SetFloat("LastY", lastY);
             anim.SetBool("Movement", false);
+            if (audioSource.isPlaying) audioSource.Stop();
         }
         else
         {
             lastX = dir.x;
             lastY = dir.y;
             anim.SetBool("Movement", true);
+            audioSource.volume = initialVolume + ConvertScale(boundary.xMin, boundary.xMax, Mathf.Abs(transform.position.x), menorVolume, maiorVolume);
+            audioSource.panStereo = ConvertScale(boundary.xMin, boundary.xMax, transform.position.x, menorPan, maiorPan);
+            if (!audioSource.isPlaying) audioSource.Play();
         }
 
         anim.SetFloat("DirX", dir.x);
         anim.SetFloat("DirY", dir.y);
+    }
+
+    private float ConvertScale(float initial, float end, float value, float initialTarget, float endTarget)
+    {
+        return ((value - initial) / (end - initial)) * (endTarget - initialTarget) + initialTarget;
     }
 }
